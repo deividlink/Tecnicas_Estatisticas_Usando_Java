4a5cb67a45c8799f16e1d8ebaff3a04e99dcc048
deividlink <deividlim312@gmail.com>
2023-07-29 20:07:48 -0300

    Código Principal

--- src/unidade_01_code/Main.java
+++ src/unidade_01_code/Main.java
@@ -13,7 +13,6 @@ public class Main {
         List<Double> Y = List.of(9.0,10.0,14.0);
         regressaoLinear.calcularBeta1(X,Y);
         regressaoLinear.calcularBeta0(X,Y,regressaoLinear.getBeta1());
-
         double x_test = 9.0;
         double predict = regressaoLinear.predictY(x_test);
         System.out.println("Regressao Linear:\n - X: " + x_test + "\n - Beta1: " + regressaoLinear.getBeta1() + "\n - Beta0: "+ regressaoLinear.getBeta0()+"\n - Predict: " +predict);
